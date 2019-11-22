827f2000c8e68dbbd4a9d76d17ae7b6ece04fc5a
WooLyung <vlfkdydtk@naver.com>
2019-11-22 09:50:25 +0900

    add apps

--- recommend_todays_dinner/recommend_todays_dinner/urls.py
+++ recommend_todays_dinner/recommend_todays_dinner/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('list/', include('list.urls')),
+    path('rank/', include('rank.urls')),
+    path('recommend/', include('recommend.urls')),
+    path('review/', include('review.urls')),
 ]
